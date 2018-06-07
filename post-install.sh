9aaaf380f659b379ecbf90cdfa3e4ac53fd18736
BigRush <tom.harpaz98@gmail.com>
2018-06-08 02:14:42 +0300

    yat

--- post-install.sh
+++ post-install.sh
@@ -70,13 +70,13 @@ Progress_Spinner () {		## progress bar that runs while the installation process 
 
 		## checks exit status of last command, if succeed
 	#	if [[ $? -eq 0 ]]; then
-			printf "\r$output_text in progress...  [|]"
+			printf "\r$output_text in progress...  [|]	"
 			sleep 0.75
-			printf "\r$output_text in progress... [/"
+			printf "\r$output_text in progress... [/]	"
 			sleep 0.75
-			printf "\r$output_text in progress... [-"
+			printf "\r$output_text in progress... [-]	"
 			sleep 0.75
-			printf "\r$output_text in progress... [\\"
+			printf "\r$output_text in progress... [\\]	"
 
 <<com
 			printf "\r\r|"
