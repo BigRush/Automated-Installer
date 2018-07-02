92ef1bc69db04bf94212deefd5f927b94178ec8f
BigRush <tom.harpaz98@gmail.com>
2018-07-02 17:44:45 +0300

    fixing main added main menu

--- Main.sh
+++ Main.sh
@@ -346,103 +346,116 @@ Dependencies_Installation
 Source_And_Validation
 
 ## Propmet the user with a menu to start the script
-IFS=","
-scripts=("Post install","Aurhelper **Run as Non-Root**","Clean Logs","Exit")
-PS3="Please choose what would you like to do: "
-select opt in ${scripts[*]} ; do
-    case $opt in
-        "Post install")
-			if [[ $Distro_Val == arch ]]; then
-				Arch_Config
-				sleep 1
-				Alias_and_Wallpaper
-				sleep 1
-				Pacman_Multilib
-				sleep 1
-				DE_Menu
-				sleep 1
-				DM_Menu
-				sleep 1
-				Boot_Manager_Config
-			fi
-			printf "$line\n"
-			printf "Aurhelper completed successfully\n"
-			printf "$line\n\n"
-			exit 0
-			;;
+Main_Menu () {
+	IFS=","
+	scripts=("Post install","Aurhelper **Run as Non-Root**","Clean Logs","Exit")
+	PS3="Please choose what would you like to do: "
+	select opt in ${scripts[*]} ; do
+	    case $opt in
+	        "Post install")
+				if [[ $Distro_Val == arch ]]; then
+					Arch_Config
+					sleep 1
+					Alias_and_Wallpaper
+					sleep 1
+					Pacman_Multilib
+					sleep 1
+					DE_Menu
+					sleep 1
+					DM_Menu
+					sleep 1
+					Boot_Manager_Config
+				fi
+				printf "$line\n"
+				printf "Aurhelper completed successfully\n"
+				printf "$line\n\n"
+				exit 0
+				;;
 
-        "Aurhelper **Run as Non-Root**")
-			Non_Root_Check
-			if [[ $Distro_Val == arch ]]; then
-<<COM
-				## Use getopts so I'll have the option to
-				## choose between aurman and yay
-				while getopts :a:h flag do;
-					case $flag in
-						a)
-						    if [[ "aurman" == "$OPTARG" ]]; then
-								Aurman_Install
-								sleep 1
-								Aurman_Applications
-								sleep 1
-								Vbox_Installation
-								exit 0
-							elif [[ "yay" == "$OPTARG" ]]; then
-								Yay_Install
-								sleep 1
-								Yay_Applications
-								sleep 1
-								Vbox_Installation
-								exit 0
-							fi
-							;;
+	        "Aurhelper **Run as Non-Root**")
+				Non_Root_Check
+				if [[ $Distro_Val == arch ]]; then
+					if [[ $aur_helper == "yay" || -z $aur_helper ]]; then
+						Yay_Install
+						sleep 1
+						Yay_Applications
+						sleep 1
+						Vbox_Installation
 
-						h)
-							printf "$line\n"
-							printf "Usage: \n-a <argumant>\tchoose which aurhelper would you like to use [aurman|yay] (yay is the default option)\n"
-							printf "$line\n\n"
-							exit 0
-							;;
+					elif [[ $aur_helper == "aurman" ]]; then
+						aur_helper="aurman"
+						Main_Menu
+						Aurman_Install
+						sleep 1
+						Aurman_Applications
+						sleep 1
+						Vbox_Installation
+					fi
+				fi
 
-						\?)
-							printf "$line\n"
-							printf "Invalid option -$OPTARG\ntry -h for help\n"
-							printf "$line\n\n"
-							exit 1
-							;;
-					esac
-				done
-COM
+				printf "$line\n"
+				printf "Aurhelper completed successfully\n"
+				printf "$line\n\n"
+				exit 0
+				;;
+
+			Exit)
+				printf "$line\n"
+				printf "Exiting, have a nice day!\n"
+				printf "$line\n"
+				exit 0
+				;;
+
+			"Clean Logs")
+				output_text="Cleaning log files"
+				error_txt="while cleaning log files"
+
+				sudo rm -rf $user_path/Automated-Installer-Log
+				status=$?
+				Exit_Status
+				;;
+
+			*)
+				printf "Invalid option\n"
+				;;
+		esac
+	done
+}
+
+## Use getopts so I'll have the option to
+## choose between aurman and yay
+while getopts :a:h flag do;
+	case $flag in
+		a)
+			if [[ "aurman" == "$OPTARG" ]]; then
+				aur_helper="aurman"
+				Main_Menu
+				Aurman_Install
+			elif [[ "yay" == "$OPTARG" ]]; then
+				aur_helper="yay"
 				Yay_Install
 				sleep 1
 				Yay_Applications
 				sleep 1
 				Vbox_Installation
 			fi
+			;;
+
+		h)
 			printf "$line\n"
-			printf "Aurhelper completed successfully\n"
+			printf "Usage: \n-a <argument>\tchoose which aurhelper would you like to use [aurman|yay] (yay is the default option)\n"
 			printf "$line\n\n"
 			exit 0
 			;;
 
-		Exit)
+		\?)
 			printf "$line\n"
-			printf "Exiting, have a nice day!\n"
-			printf "$line\n"
-			exit 0
-			;;
-
-		"Clean Logs")
-			output_text="Cleaning log files"
-			error_txt="while cleaning log files"
-
-			sudo rm -rf $user_path/Automated-Installer-Log
-			status=$?
-			Exit_Status
-			;;
-
-		*)
-			printf "Invalid option\n"
+			printf "Invalid option -$OPTARG\ntry -h for help\n"
+			printf "$line\n\n"
+			exit 1
 			;;
 	esac
 done
+
+## Call Main_Menu function if getopts did not detected any arguments
+Main_Menu
