e2f6687f3c1e844ca35fc49091e756eb31820d2d
BigRush <tom.harpaz98@gmail.com>
2018-07-03 13:10:59 +0300

    test

--- Main.sh
+++ Main.sh
@@ -448,7 +448,8 @@ while getopts :a:h flag; do
 		h)
 			printf "$line\n"
 			printf " Usage:\n -a <argument>\n"
-			printf "\t\tchoose which AURhelper you would like to use [ 'aurman' or 'yay' ]\n"
+			printf "\t\tchoose which AURhelper you\n"
+			printf "would like to use [ 'aurman' or 'yay' ]\n"
 			printf "\t\t('yay' is the default option if '-a' is not used)\n"
 			printf "$line\n\n"
 			exit 0
@@ -457,7 +458,8 @@ while getopts :a:h flag; do
 		:)
 			printf "$line\n"
 			printf " Usage:\n -a <argument>\n"
-			printf "\t\tchoose which AURhelper you would like to use  [ 'aurman' or 'yay' ]\n"
+			printf "\t\tchoose which AURhelper you\n"
+			printf "would like to use [ 'aurman' or 'yay' ]\n"
 			printf "\t\t('yay' is the default option if '-a' is not used)\n"
 			printf "$line\n\n"
 			exit 0
