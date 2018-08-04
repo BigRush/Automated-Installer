68435984b519a4f5b745689aea5118be80f4c965
BigRush <tom.harpaz98@gmail.com>
2018-08-04 18:14:30 +0300

    added aliases

--- .post-install.sh
+++ .post-install.sh
@@ -241,6 +241,20 @@ Alias_and_Wallpaper () {
 		printf "alias la='ls -la'\n" >> $user_path/.bashrc
 	fi
 
+	if [[ -z $(grep "alias syst='systemctl status'" $user_path/.bashrc) ]]; then
+		printf "alias syst='systemctl status'\n" >> $user_path/.bashrc
+	fi
+
+
+	if [[ -z $(grep "alias pls='sudo $(history -p !!)'" $user_path/.bashrc) ]]; then
+		printf "alias pls='sudo $(history -p !!)'\n" >> $user_path/.bashrc
+	fi
+
+	if [[ -z $(grep "alias fuck='pkill $1'" $user_path/.bashrc) ]]; then
+		printf "alias pls='alias fuck='pkill $1'\n" >> $user_path/.bashrc
+	fi
+
+
 	if [[ -z $(grep "screenfetch -E" $user_path/.bashrc) ]]; then
 		printf "screenfetch -E\n" >> $user_path/.bashrc
 	fi
@@ -259,6 +273,22 @@ Alias_and_Wallpaper () {
 
 	if [[ -z $(sudo grep "alias la='ls -la'" /root/.bashrc) ]]; then
 		sudo runuser -l "root" -c "printf \"alias la='ls -la'\n\" >> /root/.bashrc"
+	fi
+
+	if [[ -z $(sudo grep "alias syst='systemctl status'" /root/.bashrc) ]]; then
+		sudo runuser -l "root" -c "printf \"alias syst='systemctl status'\n\" >> /root/.bashrc"
+	fi
+
+	if [[ -z $(sudo grep "alias sysr='systemctl restart'" /root/.bashrc) ]]; then
+		sudo runuser -l "root" -c "printf \"alias sysr='systemctl restart'\n\" >> /root/.bashrc"
+	fi
+
+	if [[ -z $(sudo grep "alias syse='systemctl enable'" /root/.bashrc) ]]; then
+		sudo runuser -l "root" -c "printf \"alias syse='systemctl enable'\n\" >> /root/.bashrc"
+	fi
+
+	if [[ -z $(sudo grep "alias fuck='pkill $1'" /root/.bashrc) ]]; then
+		sudo runuser -l "root" -c "printf \"alias fuck='pkill $1'\n\" >> /root/.bashrc"
 	fi
 }
 
