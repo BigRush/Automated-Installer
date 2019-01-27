4936083303cf49f454706939a0e8a016454ca038
BigRush <tom.harpaz98@gmail.com>
2019-01-27 23:12:08 +0200

    moved arch functions to another file

--- .aurhelper.sh
+++ .aurhelper.sh
@@ -6,10 +6,9 @@
 #
 # License :	GPLv3
 #
-# Description :	Update the system
-#				Install aurman (AUR helper),
-#				Enable multilib repository for pacman
-#				Install applications that i want with aurman
+# Description :	Install applications that I want
+#				Install VirtualBox
+#
 #
 # Version :	1.0.0
 ################################################################################
@@ -17,132 +16,6 @@
 ## ToDo	####################################
 # Add verbos option
 ############################################
-
-## Install aurman manually
-Aurman_Install () {
-
-	## Propmet the user with what the script will now do (with cosmetics :D)
-	printf "$line\n"
-	printf "Updating the system...\n"
-	printf "$line\n\n"
-
-	## Will be used in Exit_Status function to output text for the user
-	output_text="Update"
-	error_txt="while updating"
-
-	## Update the system, send stdout and sterr to log files
-	sudo pacman -Syu 2>> $errorpath >> $outputpath &
-	status=$?
-	Progress_Spinner
-	Exit_Status
-
-	## Create a tmp-working-dir if it does't exits and navigate into it
-	if ! [[ -e $user_path/Automated-Installer-Log/aurman_install_tmp ]]; then
-		mkdir -p $user_path/Automated-Installer-Log/aurman_install_tmp
-	fi
-
-	pushd . 2>> $errorpath >> $outputpath
-	cd $user_path/Automated-Installer-Log/aurman_install_tmp
-
-	## Check if "aurman" exists, if not, install "aurman" from AUR
-	if [[ -z $(command -v aurman) ]]; then
-		output_text="Getting aurman with curl from AUR"
-		error_txt="while getting aurman with curl from AUR"
-
-		## Get the build files for AUR
-    	curl -s -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/aurman.tar.gz 2>> $errorpath >> $outputpath &
-		BPID=$!
-		Progress_Spinner
-		wait $BPID
-		status=$?
-		Exit_Status
-
-		tar -xf aurman.tar.gz 2>> $errorpath >> $outputpath
-
-		cd aurman
-
-		output_text="Aurman building"
-		error_txt="while building aurman"
-
-		## Add gpg key
-		gpg --recv-keys 465022E743D71E39 2>> $errorpath >> $outputpath
-
-		## Compile
-		makepkg -si PKGBUILD --noconfirm --needed 2>> $errorpath >> $outputpath &
-		BPID=$!
-		Progress_Spinner
-		wait $BPID
-		status=$?
-		Exit_Status
-	fi
-
-	## Clean up on aisle four
-	popd 2>> $errorpath >> $outputpath
-	rm -rf $user_path/aurman_install_tmp
-}
-
-## Install yay manually
-Yay_Install () {
-
-	## Propmet the user with what the script will now do (with cosmetics :D)
-	printf "$line\n"
-	printf "Updating the system...\n"
-	printf "$line\n\n"
-
-	## Will be used in Exit_Status function to output text for the user
-	output_text="Update"
-	error_txt="while updating"
-
-	## Update the system, send stdout and sterr to log files
-	sudo pacman -Syu 2>> $errorpath >> $outputpath &
-	status=$?
-	Progress_Spinner
-	Exit_Status
-
-	## Create a tmp-working-dir if it does't exits and navigate into it
-	if ! [[ -e $user_path/Automated-Installer-Log/yay_install_tmp ]]; then
-		mkdir -p $user_path/Automated-Installer-Log/yay_install_tmp
-	fi
-
-	pushd . 2>> $errorpath >> $outputpath
-	cd $user_path/Automated-Installer-Log/yay_install_tmp
-
-	## Check if "yay" exists, if not, install "yay" from AUR
-	if ! [[ -n "$(pacman -Qs yay)" ]]; then
-		output_text="getting yay with curl from AUR"
-		error_txt="while getting yay with curl from AUR"
-
-		## Get the build files for AUR
-    	curl -s -L -O https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz 2>> $errorpath >> $outputpath &
-		BPID=$!
-		Progress_Spinner
-		wait $BPID
-		status=$?
-		Exit_Status
-
-		tar -xf yay.tar.gz 2>> $errorpath >> $outputpath
-
-		cd yay
-
-		output_text="yay building"
-		error_txt="while building yay"
-
-		## Add gpg key
-		# gpg --recv-keys 465022E743D71E39 2>> $errorpath >> $outputpath
-
-		## Compile
-		makepkg -si PKGBUILD --noconfirm --needed 2>> $errorpath >> $outputpath &
-		BPID=$!
-		Progress_Spinner
-		wait $BPID
-		status=$?
-		Exit_Status
-	fi
-
-	## Clean up on aisle four
-	popd 2>> $errorpath >> $outputpath
-	rm -rf $user_path/yay_install_tmp
-}
 
 ## Applications I want to install with aurman
 Aurman_Applications () {
@@ -175,10 +48,6 @@ Aurman_Applications () {
 			Exit_Status
 		fi
 }
-
-## steam 32 lib
-# sudo apt install libgl1-mesa-dri:i386 libgl1-mesa-glx:i386
-#sudo apt install lib32stdc++6
 
 ## Applications I want to install with yay
 Yay_Applications () {
@@ -225,8 +94,6 @@ Apt_Applications () {
 ## Applications that needs to be installed from .deb files
 Deb_Packages () {
 
-	dpkg_applications=( atom etcher)
-
 	##  Discord installation ##
 
 	## Check if Downloads folder exists and if not, create it
@@ -240,8 +107,10 @@ Deb_Packages () {
 	error_txt="while downloading Discord's .deb package"
 
 	curl -L -o $user_path/Downloads/discord.deb https://discordapp.com/api/download?platform=linux&format=deb 2>> $errorpath >> $outputpath &
-	status=$?
+	BPID=$!
 	Progress_Spinner
+	wait $BPID
+	status=$?
 	Exit_Status
 
 	## Installing Discord from .deb package
@@ -255,8 +124,10 @@ Deb_Packages () {
 	error_txt="while installing Discord"
 
 	sudo apt install $user_path/Downloads/discord.deb -y 2>> $errorpath >> $outputpath &
-	status=$?
+	BPID=$!
 	Progress_Spinner
+	wait $BPID
+	status=$?
 	Exit_Status
 
 	## Steam installation ##
@@ -267,8 +138,10 @@ Deb_Packages () {
 	error_txt="while downloading Steam's .deb package"
 
 	curl -L -o $user_path/Downloads/steam.deb https://steamcdn-a.akamaihd.net/client/installer/steam.deb 2>> $errorpath >> $outputpath &
-	status=$?
+	BPID=$!
 	Progress_Spinner
+	wait $BPID
+	status=$?
 	Exit_Status
 
 	## Adding i386 architecture
@@ -285,11 +158,13 @@ Deb_Packages () {
 	## Update the package lists
 
 	output_text="Updating the package lists"
-	error_txt="Updating the package lists"
+	error_txt="while updating the package lists"
 
 	apt-get update 2>> $errorpath >> $outputpath &
-	status=$?
+	BPID=$!
 	Progress_Spinner
+	wait $BPID
+	status=$?
 	Exit_Status
 
 	## Installing Steam from .deb package
@@ -300,8 +175,10 @@ Deb_Packages () {
 	sudo echo
 
 	sudo apt install $user_path/Downloads/steam.deb -y 2>> $errorpath >> $outputpath &
-	status=$?
+	BPID=$!
 	Progress_Spinner
+	wait $BPID
+	status=$?
 	Exit_Status
 
 	## Installing TeamViewer ##
@@ -312,8 +189,10 @@ Deb_Packages () {
 	error_txt="while downloading TeamViewer's .deb package"
 
 	curl -L -o $user_path/Downloads/teamviewer.deb https://download.teamviewer.com/download/linux/teamviewer_amd64.deb 2>> $errorpath >> $outputpath &
-	status=$?
+	BPID=$!
 	Progress_Spinner
+	wait $BPID
+	status=$?
 	Exit_Status
 
 	## Installing TeamViewer from .deb package
@@ -327,8 +206,10 @@ Deb_Packages () {
 	error_txt="while installing TeamViewer"
 
 	sudo apt install $user_path/Downloads/teamviewer.deb -y 2>> $errorpath >> $outputpath &
-	status=$?
+	BPID=$!
 	Progress_Spinner
+	wait $BPID
+	status=$?
 	Exit_Status
 
 	## Installing Atom ##
@@ -339,8 +220,10 @@ Deb_Packages () {
 	error_txt="while downloading Atom's .deb package"
 
 	curl -L -o $user_path/Downloads/atom.deb 	https://atom.io/download/deb 2>> $errorpath >> $outputpath &
-	status=$?
+	BPID=$!
 	Progress_Spinner
+	wait $BPID
+	status=$?
 	Exit_Status
 
 	## Installing Atom from .deb package
@@ -354,18 +237,61 @@ Deb_Packages () {
 	error_txt="while installing Atom"
 
 	sudo apt install $user_path/Downloads/atom.deb -y 2>> $errorpath >> $outputpath &
-	status=$?
+	BPID=$!
 	Progress_Spinner
+	wait $BPID
+	status=$?
+	Exit_Status
+
+	## Installing Etcher ##
+
+	## Add Etcher's debian repository
+
+	output_text="Adding Etcher's debian repository"
+	error_txt="while adding Etcher's debian repository"
+
+	echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list 2>> $errorpath >> $outputpath
+	status=$?
 	Exit_Status
 
 
+	## Add Bintray.com's GPG key
+	output_text="Adding Bintray.com's GPG key"
+	error_txt="while adding Bintray.com's GPG key"
+
+	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61 2>> $errorpath >> $outputpath
+	status=$?
+	Exit_Status
+
+	## Update the package lists
+
+	output_text="Updating the package lists"
+	error_txt="while updating the package lists"
+
+	apt-get update 2>> $errorpath >> $outputpath &
+	BPID=$!
+	Progress_Spinner
+	wait $BPID
+	status=$?
+	Exit_Status
+
+	## Installing Etcher
+
+	output_text="Installing Etcher"
+	error_txt="while installing Etcher"
+
+	sudo echo
+
+	sudo apt install balena-etcher-electron -y 2>> $errorpath >> $outputpath &
+	BPID=$!
+	Progress_Spinner
+	wait $BPID
+	status=$?
+	Exit_Status
 }
-
-
 
 ## Virtualbox installation
 Vbox_Installation () {
-
 	read -p "Would you like to install virtualbox?[Y/n]: " answer
 	printf "\n"
 	if [[ -z $answer ]]; then
@@ -384,56 +310,133 @@ Vbox_Installation () {
 		printf "$line\n\n"
 	fi
 
-	vbox_pkg=(virtualbox virtualbox-host-modules-arch linux-headers virtualbox-ext-oracle)
 
-	## Check which AUR helper is installed
-	if [[ $aur_helper == "aurman" ]]; then
-		for i in ${vbox_pkg[*]}; do
+	if [[ $Distro_Val == arch || $Distro_Val == manjaro ]]; then
+		vbox_pkg=(virtualbox virtualbox-host-modules-arch linux-headers virtualbox-ext-oracle)
+
+		## Check which AUR helper is installed
+		if [[ $aur_helper == "aurman" ]]; then
+			for i in ${vbox_pkg[*]}; do
+				printf "$line\n"
+				printf "Installing $i\n"
+				printf "$line\n\n"
+				output_text="$i installation"
+				error_txt="while installing $i"
+				aurman -S $i --needed --noconfirm --noedit 2>> $errorpath >> $outputpath &
+				BPID=$!
+				Progress_Spinner
+				wait $BPID
+				status=$?
+				Exit_Status
+			done
+
+		elif [[ $aur_helper == "yay" ]]; then
+			for i in ${vbox_pkg[*]}; do
+				printf "$line\n"
+				printf "Installing $i\n"
+				printf "$line\n\n"
+				output_text="$i installation"
+				error_txt="while installing $i"
+				yay -S $i --needed --noconfirm 2>> $errorpath >> $outputpath &
+				BPID=$!
+				Progress_Spinner
+				wait $BPID
+				status=$?
+				Exit_Status
+			done
+
+		else
 			printf "$line\n"
-			printf "Installing $i\n"
-			printf "$line\n\n"
-			output_text="$i installation"
-			error_txt="while installing $i"
-			aurman -S $i --needed --noconfirm --noedit 2>> $errorpath >> $outputpath &
-			status=$?
-			Progress_Spinner
-			Exit_Status
-		done
+			printf "No AUR helper detected, can't install  virtualbox extension pack,\n"
+			printf "Please install it manually later... Continueing with the Vbox installation...\n"
 
-	elif [[ $aur_helper == "yay" ]]; then
-		for i in ${vbox_pkg[*]}; do
-			printf "$line\n"
-			printf "Installing $i\n"
-			printf "$line\n\n"
-			output_text="$i installation"
-			error_txt="while installing $i"
-			yay -S $i --needed --noconfirm 2>> $errorpath >> $outputpath &
-			status=$?
-			Progress_Spinner
-			Exit_Status
-		done
+			vbox_pkg_pac=(virtualbox virtualbox-host-modules-arch linux-headers)
+			for i in ${vbox_pkg_pac[*]}; do
+				printf "$line\n"
+				printf "Installing $i\n"
+				printf "$line\n\n"
+				output_text="$i installation"
+				error_txt="while installing $i"
+				pacman -S $i --needed --noconfirm 2>> $errorpath >> $outputpath &
+				BPID=$!
+				Progress_Spinner
+				wait $BPID
+				status=$?
+				Exit_Status
+			done
+		fi
 
-	else
-		printf "$line\n"
-		printf "No AUR helper detected, can't install  virtualbox extension pack,\n"
-		printf "Please install it manually later... Continueing with the Vbox installation...\n"
+		sudo modprobe vboxdrv 2>> $errorpath >> $outputpath
+		Exit_Status
+		sudo gpasswd -a $orig_user vboxusers 2>> $errorpath >> $outputpath
+		Exit_Status
 
-		vbox_pkg_pac=(virtualbox virtualbox-host-modules-arch linux-headers)
-		for i in ${vbox_pkg_pac[*]}; do
-			printf "$line\n"
-			printf "Installing $i\n"
-			printf "$line\n\n"
-			output_text="$i installation"
-			error_txt="while installing $i"
-			pacman -S $i --needed --noconfirm 2>> $errorpath >> $outputpath &
-			status=$?
-			Progress_Spinner
-			Exit_Status
-		done
-	fi
+	elif [[ $Distro_Val == debian || $Distro_Val == \"Ubuntu\" ]]; then
 
-	sudo modprobe vboxdrv 2>> $errorpath >> $outputpath
-	Exit_Status
-	sudo gpasswd -a tom vboxusers 2>> $errorpath >> $outputpath
-	Exit_Status
+		## Add the repository key
+
+		output_text="Adding the repository key"
+		error_txt="while adding the repository key"
+
+		wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add - 2>> $errorpath >> $outputpath &
+		BPID=$!
+		Progress_Spinner
+		wait $BPID
+		status=$?
+		Exit_Status
+
+		output_text="Adding the second repository key"
+		error_txt="while adding the second repository key"
+
+		wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add - 2>> $errorpath >> $outputpath &
+		BPID=$!
+		Progress_Spinner
+		wait $BPID
+		status=$?		BPID=$!
+		Exit_Status
+
+		## Add the VirtualBox repository
+
+		output_text="Adding the VirtualBox repository"
+		error_txt="while adding the VirtualBox repository"
+
+		echo "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -sc) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list 2>> $errorpath >> $outputpath
+		status=$?
+		Exit_Status
+
+		## Update the package lists
+
+		output_text="Updating the package lists"
+		error_txt="while ufpdating the package lists"
+
+		apt-get update 2>> $errorpath >> $outputpath &
+		BPID=$!
+		Progress_Spinner
+		wait $BPID
+		status=$?
+		Exit_Status
+
+		## Installing headers
+
+		output_text="Installing linux-headers"
+		error_txt="while installing linux-headers"
+
+		sudo apt install linux-headers-$(uname -r) dkms -y 2>> $errorpath >> $outputpath &
+		BPID=$!
+		Progress_Spinner
+		wait $BPID
+		status=$?
+		Exit_Status
+
+		## Installing VirtualBox
+
+		output_text="Installing VirtualBox"
+		error_txt="while installing VirtualBox"
+
+		sudo apt install VirtualBox -y 2>> $errorpath >> $outputpath &
+		BPID=$!
+		Progress_Spinner
+		wait $BPID
+		status=$?
+		Exit_Status
 }
--- .post-install.sh
+++ .post-install.sh
@@ -7,9 +7,12 @@
 # License :	GPLv3
 #
 # Description :	Update the system
-#				Install video card driver for intel
-#				Install Desktop environment and a display manager
-#				Add personal aliases and a nice wallpaper
+#				Add aliases and download a nice wallpaper
+#				Install desktop environment
+#				Download themes and icons
+#				Install display manager
+#				Configure the grub background and fast boot time
+#
 # Version :	1.0.0
 ################################################################################
 
@@ -20,191 +23,6 @@
 
 ####  Functions  ###############################################################
 
-## Configure arch after a clean install with KDE desktop environment
-Arch_Config () {
-
-	## Prompet sudo
-	sudo echo
-	## Propmet the user with what the script will now do (with cosmetics :D)
-	printf "$line\n"
-	printf "Updating the system...\n"
-	printf "$line\n\n"
-
-	## Will be used in Exit_Status function to output text for the user
-	output_text="Update"
-	error_txt="while updating"
-
-	## Update the system, send stdout, sterr to log files
-	## and move the process to the background for the Progress_Spinner function.
-	sudo pacman -Syu --noconfirm 2>> $errorpath >> $outputpath &
-
-	## Save the background PID to a variable for later use with wait command
-	BPID=$!
-
-	## Call Progress_Spinner function
-	Progress_Spinner
-
-	## Wait until the process is done to get its exit status.
-	wait $BPID
-
-	## Save the exxit status of last command to a Varibale
-	status=$?
-
-	## Call Exit_Status function
-	Exit_Status
-
-
-	## Understanding the logic behind the code
-	############################################################################
-	## If I don't use this method of:
-	## 1. Sending the process to the backgroud.
-	## 2. Save its PID.
-	## 3. Call Progress_Spinner function
-	## 4. Executing wait command.
-	## 5. Save the process exit status.
-	## 6. Make sure the exit status is 0.
-	##
-	## I will not be able to:
-	## 1. Run the Progress_Spinner function because it
-	##    depends on the backgroung PID of the last executed command.
-	##
-	## 2. Make sure the command executed successfully or not, because when you
-	##    you compare the exit status of the process that has been sent to the
-	##    the background, you will be getting the exit status of a different
-	##    command, in this case I got the exit status of the declaration of
-	##    "error_txt" variable (before implementing this method of wait command,
-	##    it can be seen on early commits).
-	##
-	## So now by waiting until the process is done, I can safely check the exit
-	## status, because the wait command will return an exit status according to
-	## the success of the process its given.
-	############################################################################
-
-	sudo echo
-
-	## Wait for 0.5 seconds for preventing unwanted errors
-	# sleep 0.5
-
-	printf "$line\n"
-	printf "Installing Xorg...\n"
-	printf "$line\n\n"
-
-	output_text="Xorg installation"
-	error_txt=" while installing Xorg"
-	sudo pacman -S xorg xorg-xinit --needed --noconfirm 2>> $errorpath >> $outputpath &
-	BPID=$!
-	Progress_Spinner
-	wait $BPID
-	status=$?
-	Exit_Status
-
-	# sleep 0.5
-
-	## Make sure there is an Intel video card and install its drivers.
-	## If no Intel video card detected,
-	## tell the user and continue the script
-	lspci |grep VGA |grep Intel &> /dev/null
-	if [[ $? -eq 0 ]]; then
-		printf "$line\n"
-		printf "Installing video drivers...\n"
-		printf "$line\n\n"
-
-		output_text="Video card drivers installationl"
-		error_txt="while installing video card's drivers"
-
-		sudo echo
-
-		sudo pacman -S xf86-video-intel --needed --noconfirm 2>> $errorpath >> $outputpath &
-		Progress_Spinner
-		BPID=$!
-		wait $BPID
-		status=$?
-		Exit_Status
-		# sleep 0.5
-	else
-		printf "$line\n"
-		printf "Did not detect Intel video card,\n"
-		printf "please install video card drivers by yourself later.\n"
-		printf "Continuing with the script...\n"
-		printf "$line\n\n"
-		# sleep 2
-	fi
-}
-
-## Enable multilib repo
-Pacman_Multilib () {
-
-	## Validate the multilib section is in the place that we are going to replace
-
-	## Set conf file under a variable
-	pac_path=/etc/pacman.conf
-	## If you can't find '#[multilib]'
-	if ! [[ -z $(cat $pac_path |egrep "^\#\[multilib\]$") ]]; then
-		## Set line counter to 1
-		i=1
-		## On each loop increase i value by 1 until 100
-		for ((i; i<=100; i++)); do
-			## Set the output of line "i" unser a variable
-			pac_line=$(sed -n "$i"p $pac_path)
-			## Check if "#[multilib]" is in that line,
-			## then check if it's line 93 (because i know that the specific
-			## "#[multilib]" should be in line 93 for the time i wrote this),
-			## If it's on line 93 remove the first characters in lines 93 & 94
-			## (which will be '#') to apply multilib repo, then break the loop,
-			## If it's not on line 93 tell the user that file probably changed
-			## and he should do it manually,
-			## If it didn't find it at all then tell the user: multib failed
-			if [[ "#[multilib]" == "$pac_line" ]]; then
-				if [[ $i -eq 93 ]]; then
-					sudo sed -ie "93,94s/.//" $pac_path
-					break
-
-				else
-					printf "$line\n"
-					printf "the pacman.conf file has changed its format\nplease enable multilib for pacman so the script will run correctly\nnot applying any chnages\n"
-					printf "$line\n\n"
-					break
-				fi
-			elif [[ $i -eq 100 && ! "#[multilib]" == "$pac_line" ]];then
-				printf "$line\n"
-				printf "Adding multilib repo failed...\n"
-				printf "$line\n\n"
-
-				read -p "Would you like to continue anyway?[y/N]: " answer
-				printf "\n"
-				if [[ -z $answer ]]; then
-					exit 1
-				elif [[ $answer =~ [y|Y] || $answer =~ [y|Y]es ]]; then
-					:
-				elif [[ $answer =~ [n|N] || $answer =~ [n|N]o ]]; then
-					printf "$line\n"
-					printf "Exiting...\n"
-					printf "$line\n\n"
-					exit 1
-				else
-					printf "$line\n"
-					printf "Invalid answer - exiting\n"
-					printf "$line\n\n"
-					exit 1
-				fi
-			fi
-		done
-
-		printf "$line\n"
-		printf "Syncing multilib...\n"
-		printf "$line\n\n"
-
-		output_text="Multilib sync"
-		error_txt="while syncing multilib"
-
-		sudo pacman -Sy 2>> $errorpath >> $outputpath &
-		Progress_Spinner
-		BPID=$!
-		wait $BPID
-		status=$?
-		Exit_Status
-	fi
-}
 
 ## Add aliases and download a nice wallpaper
 Alias_and_Wallpaper () {
@@ -327,30 +145,6 @@ Alias_and_Wallpaper () {
 	printf "$line\n\n"
 }
 
-## Cinnamon theme configuration
-Cinnamon_Theme_Config () {
-
-	sudo echo
-
-	output_text="Font installation"
-	error_txt="while installting fonts"
-
-	## Install some nice fonts
-	sudo pacman -S ttf-dejavu ttf-liberation noto-fonts --needed --noconfirm 2>> $errorpath >> $outputpath &
-	BPID=$!
-	Progress_Spinner
-	wait $BPID
-	status=$?
-	Exit_Status
-
-	sudo sh -c "echo 'deb http://ppa.launchpad.net/papirus/papirus/ubuntu bionic main' > /etc/apt/sources.list.d/papirus-ppa.list"
-
-	sudo apt-get install dirmngr
-	sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com E58A9D36647CAE7F
-	sudo apt-get update
-	sudo apt-get install papirus-icon-theme
-}
-
 ## Menu, to choose which desktop environment to install
 DE_Menu () {
 
@@ -422,59 +216,7 @@ KDE_Installation () {
 	de_env="kde"
 }
 
-## Configure ugly arch kde fonts
-KDE_Font_Config () {
-
-	sudo echo
-
-	output_text="Font installation"
-	error_txt="while installting fonts"
-
-	## Install some nice fonts
-	sudo pacman -S ttf-dejavu ttf-liberation noto-fonts --needed --noconfirm 2>> $errorpath >> $outputpath &
-	BPID=$!
-	Progress_Spinner
-	wait $BPID
-	status=$?
-	Exit_Status
-
-	## Enable font presets by creating symbolic links
-	## It will disable embedded bitmap for all fonts
-	## Enable sub-pixel RGB rendering
-	## Enable the LCD filter which is designed to reduce colour fringing when subpixel rendering is used.
-	sudo ln -sf /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
-	sudo ln -sf /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
-	sudo ln -sf /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
-
-	sudo sed -ie "s/\#export.*/export FREETYPE_PROPERTIES=\"truetype:interpreter-version=40\"/" /etc/profile.d/freetype2.sh
-
-	printf '
-	<?xml version="1.0"?>
-	<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-	<fontconfig>
-	    <match>
-	        <edit mode="prepend" name="family"><string>Noto Sans</string></edit>
-	    </match>
-	    <match target="pattern">
-	        <test qual="any" name="family"><string>serif</string></test>
-	        <edit name="family" mode="assign" binding="same"><string>Noto Serif</string></edit>
-	    </match>
-	    <match target="pattern">
-	        <test qual="any" name="family"><string>sans-serif</string></test>
-	        <edit name="family" mode="assign" binding="same"><string>Noto Sans</string></edit>
-	    </match>
-	    <match target="pattern">
-	        <test qual="any" name="family"><string>monospace</string></test>
-	        <edit name="family" mode="assign" binding="same"><string>Noto Mono</string></edit>
-	    </match>
-	</fontconfig>
-	' > $(pwd)/tmp_font
-
-	sudo runuser -l "root" -c "cat $(pwd)/tmp_font > /etc/fonts/local.conf"
-	rm tmp_font
-}
-
-## Download themes and icons for KDE
+## Download themes and icons
 Theme_Config () {
 
 	## Check Desktop environment
