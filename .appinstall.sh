888f217e1da150474dcfb32ec4d08dfead4289d4
BigRush <tom.harpaz98@gmail.com>
2019-01-31 02:17:26 +0200

    test

--- .appinstall.sh
+++ .appinstall.sh
@@ -107,12 +107,13 @@ Deb_Packages () {
 	error_txt="while downloading Discord's .deb package"
 
 	#wget -b -a $errorpath https://discordapp.com/api/download?platform=linux&format=deb
-	wget -b -o wget.log-O "$user_path/Downloads/discord.deb" https://discordapp.com/api/download?platform=linux&format=deb &#2>> $errorpath >> $outputpath &
+	wget -b -o wget.log -O "$user_path/Downloads/discord.deb" https://discordapp.com/api/download?platform=linux&format=deb &#2>> $errorpath >> $outputpath &
 	BPID=$(awk -F "pid" '{print $2}' tmp |tr -d "[:blank:][:punct:]")
 	sleep 5
 	Progress_Spinner
 	wait $BPID
 	status=$?
+	rm wget.log
 	Exit_Status
 
 
