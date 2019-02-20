382807324d78d1e39dcfe1d9fbb5969ccb98a4d1
BigRush <tom.harpaz98@gmail.com>
2019-02-20 18:42:28 +0200

    Version 2

--- Main.sh
+++ Main.sh
@@ -9,7 +9,7 @@
 # Description :  Main script that calls the appropriate functions
 #                from the other scripts
 #
-# Version :  1.0.0
+# Version :  2.0.0
 ################################################################################
 
 
